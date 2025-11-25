9e544586cf225ccf2080d860589812b464060a5b
sjlee9908 <sjlee9908@naver.com>
2025-11-25 13:23:37 +0900

    feats : config

--- main.py
+++ main.py
@@ -12,94 +12,108 @@ import torch.nn.functional as F
 from torch.autograd import grad
 import torchvision
 from torchvision import models, datasets, transforms
+import yaml
+
 print(torch.__version__, torchvision.__version__)
 
-from utils import label_to_onehot, cross_entropy_for_onehot
+from utils import *
 
-parser = argparse.ArgumentParser(description='Deep Leakage from Gradients.')
-parser.add_argument('--index', type=int, default="25",
-                    help='the index for leaking images on CIFAR.')
-parser.add_argument('--image', type=str,default="",
-                    help='the path to customized image.')
-args = parser.parse_args()
+def main(config, args):
+    # init
+    torch.manual_seed(1234)
+    tt = transforms.ToPILImage()
+    dst = datasets.CIFAR100("./data/torch", download=True)
+    device = set_device()
+    criterion = cross_entropy_for_onehot
 
-device = "cpu"
-if torch.cuda.is_available():
-    device = "cuda"
-print("Running on %s" % device)
+    # load config
+    img_index = args.index
+    batch_size = config['batch_size']
+    epoch = config['epoch']
 
-dst = datasets.CIFAR100("~/.torch", download=True)
-tp = transforms.ToTensor()
-tt = transforms.ToPILImage()
+    # load data    
+    gt_data, gt_label, gt_onehot_label = make_batched_input(dst, batch_size, img_index)
+    gt_data = gt_data.to(device)
+    gt_label = gt_label.to(device)
+    gt_onehot_label = gt_onehot_label.to(device)
 
-img_index = args.index
-gt_data = tp(dst[img_index][0]).to(device)
+    # create model
+    net = get_model(config['model'], device)
 
-if len(args.image) > 1:
-    gt_data = Image.open(args.image)
-    gt_data = tp(gt_data).to(device)
+    # compute original gradient 
+    pred = net(gt_data)
+    y = criterion(pred, gt_onehot_label)
+    dy_dx = torch.autograd.grad(y, net.parameters())
+    original_dy_dx = list((_.detach().clone() for _ in dy_dx))
 
-
-gt_data = gt_data.view(1, *gt_data.size())
-gt_label = torch.Tensor([dst[img_index][1]]).long().to(device)
-gt_label = gt_label.view(1, )
-gt_onehot_label = label_to_onehot(gt_label)
-
-plt.imshow(tt(gt_data[0].cpu()))
-
-from models.vision import LeNet, weights_init
-net = LeNet().to(device)
-
-
-torch.manual_seed(1234)
-
-net.apply(weights_init)
-criterion = cross_entropy_for_onehot
-
-# compute original gradient 
-pred = net(gt_data)
-y = criterion(pred, gt_onehot_label)
-dy_dx = torch.autograd.grad(y, net.parameters())
-
-original_dy_dx = list((_.detach().clone() for _ in dy_dx))
-
-# generate dummy data and label
-dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
-dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
-
-plt.imshow(tt(dummy_data[0].cpu()))
-
-optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
-
-
-history = []
-for iters in range(300):
-    def closure():
-        optimizer.zero_grad()
-
-        dummy_pred = net(dummy_data) 
-        dummy_onehot_label = F.softmax(dummy_label, dim=-1)
-        dummy_loss = criterion(dummy_pred, dummy_onehot_label) 
-        dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
-        
-        grad_diff = 0
-        for gx, gy in zip(dummy_dy_dx, original_dy_dx): 
-            grad_diff += ((gx - gy) ** 2).sum()
-        grad_diff.backward()
-        
-        return grad_diff
+    # generate dummy data and label
+    dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
+    dummy_label = torch.randn(gt_onehot_label.size()).to(device).requires_grad_(True)
     
-    optimizer.step(closure)
-    if iters % 10 == 0: 
-        current_loss = closure()
-        print(iters, "%.4f" % current_loss.item())
-        history.append(tt(dummy_data[0].cpu()))
+    init_dummy_data = dummy_data.detach().clone()
 
-plt.figure(figsize=(12, 8))
-for i in range(30):
-    plt.subplot(3, 10, i + 1)
-    plt.imshow(history[i])
-    plt.title("iter=%d" % (i * 10))
-    plt.axis('off')
+    
+    # create optim
+    optimizer = get_optim(config['optim'], dummy_data, dummy_label)
 
-plt.show()
+    for iters in range(epoch):
+        def closure():
+            optimizer.zero_grad()
+
+            #dummy forward
+            dummy_pred = net(dummy_data) 
+            dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+            dummy_loss = criterion(dummy_pred, dummy_onehot_label) 
+            dummy_dy_dx = torch.autograd.grad(dummy_loss, net.parameters(), create_graph=True)
+            
+            # dummy backward, model backward
+            # dummy_dy_dx가 origin_dy_dx와 비슷해지는 방향으로 step하도록함
+            grad_diff = 0
+            for gx, gy in zip(dummy_dy_dx, original_dy_dx): 
+                grad_diff += ((gx - gy) ** 2).sum()
+            grad_diff.backward()
+            
+            return grad_diff
+        
+        #dummy_dy_dx가 origin_dy_dx와 비슷해지는 방향으로 step / dummy_data, dummy_label에 대해 step
+        optimizer.step(closure)
+        if iters % 10 == 0: 
+            current_loss = closure()
+            print(iters, "%.4f" % current_loss.item())
+
+    #show
+    plt.figure(figsize=(2 * batch_size, 6))
+    
+    for i in range(batch_size):
+        plt.subplot(3, batch_size, i + 1)
+        plt.imshow(tt(gt_data[i].cpu()))
+        plt.axis('off')
+        if i == 0: plt.title("Original")
+
+        plt.subplot(3, batch_size, batch_size + i + 1)
+        plt.imshow(tt(init_dummy_data[i].cpu()))
+        plt.axis('off')
+        if i == 0: plt.title("Initial")
+
+        plt.subplot(3, batch_size, 2 * batch_size + i + 1)
+        plt.imshow(tt(dummy_data[i].detach().cpu()))
+        plt.axis('off')
+        if i == 0: plt.title("Final")
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Deep Leakage from Gradients.')
+    parser.add_argument('--index', type=int, default=400,
+                        help='the index for leaking images on CIFAR.')
+    parser.add_argument('--config', type=str, default="./config.yaml",
+                        help='the path to yaml config file.')
+    args = parser.parse_args()
+
+
+    with open(args.config) as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+
+    main(config, args)
--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 import torch
 import torch.nn.functional as F
 
+import numpy as np
+from pprint import pprint
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import grad
+import torchvision
+from torchvision import models, datasets, transforms
+import yaml
+
+
 
 def label_to_onehot(target, num_classes=100):
     target = torch.unsqueeze(target, 1)
@@ -10,3 +25,102 @@ def label_to_onehot(target, num_classes=100):
 
 def cross_entropy_for_onehot(pred, target):
     return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
+
+
+def set_device():
+    device = "cpu"
+    if torch.cuda.is_available(): device = "cuda"
+    print("Running on %s" % device)
+    return device
+
+
+def make_batched_input(dataset, batch_size, start_idx):
+    tp = transforms.ToTensor()
+
+    gt_data_list = []
+    gt_label_list = []
+    for i in range(batch_size):
+        current_idx = (start_idx + i) % len(dataset)
+        data, label = dataset[current_idx]
+        data = tp(data)
+        gt_data_list.append(data)
+        gt_label_list.append(torch.tensor(label))
+
+    gt_data = torch.stack(gt_data_list)
+    gt_label = torch.stack(gt_label_list).long()
+    gt_onehot_label = label_to_onehot(gt_label, num_classes=100)
+
+    return gt_data, gt_label, gt_onehot_label
+
+
+def get_model(model, device):
+        
+    # create, init global model
+    if model == "lenet":
+        from models.lenet import LeNet, weights_init
+        net = LeNet().to(device)
+        net.apply(weights_init)
+    elif model == 'resnet18':
+        from models.resnet import resnet18, weights_init
+        net = resnet18().to(device)
+        net.apply(weights_init)
+    elif model == 'resnet34':
+        from models.resnet import resnet34, weights_init
+        net = resnet34().to(device)
+        net.apply(weights_init)
+    elif model == 'resnet50':
+        from models.resnet import resnet101, weights_init
+        net = resnet101().to(device)
+        net.apply(weights_init)
+    elif model == 'resnet101':
+        from models.resnet import resnet152, weights_init
+        net = resnet152().to(device)
+        net.apply(weights_init)
+    elif model == 'resnet152':
+        from models.resnet import resnet152, weights_init
+        net = resnet152().to(device)
+        net.apply(weights_init)
+    
+    return net
+
+def get_optim(optim, dummy_data, dummy_label):
+    lr = 1
+    params = [dummy_data, dummy_label]
+
+    if optim == "LBFGS":
+        optimizer = torch.optim.LBFGS(params)
+        
+    elif optim == "AdamW":
+        optimizer = torch.optim.AdamW(params, lr=lr)
+        
+    elif optim == "Adam":
+        optimizer = torch.optim.Adam(params, lr=lr)
+        
+    elif optim == "SGD":
+        optimizer = torch.optim.SGD(params, lr=lr, momentum=0.9)
+        
+    elif optim == "RMSprop":
+        optimizer = torch.optim.RMSprop(params, lr=lr)
+        
+    elif optim == "Adadelta":
+        optimizer = torch.optim.Adadelta(params, lr=lr)
+        
+    elif optim == "Adagrad":
+        optimizer = torch.optim.Adagrad(params, lr=lr)
+        
+    elif optim == "Adamax":
+        optimizer = torch.optim.Adamax(params, lr=lr)
+        
+    elif optim == "NAdam":
+        optimizer = torch.optim.NAdam(params, lr=lr)
+        
+    elif optim == "RAdam":
+        optimizer = torch.optim.RAdam(params, lr=lr)
+        
+    else:
+        raise ValueError(f"Unknown optimizer: {optim}")
+    
+    return optimizer
+
+
+# def ceptual_cos_sim():
